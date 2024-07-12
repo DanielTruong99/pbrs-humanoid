2f219c1c53d2f179c5643c6d59cedc9ac5f73129
DanielTruong99 <datthanh01237344449@gmail.com>
2024-07-12 15:52:49 +0900

    Update test scripts for plotting recorded simulation datas

--- gpugym/analysis/data/test.py
+++ gpugym/analysis/data/test.py
@@ -17,37 +17,64 @@ DOF_NAMES = [
 file_name = 'gpugym/analysis/data/play_log_leg_2.csv'
 df = pd.read_csv(file_name)
 
-# fig, axs = plt.subplots(3, 1, figsize=(10, 8))
+def plot_base_velocity():
+    fig, axs = plt.subplots(3, 1, figsize=(10, 8))
 
-# df.plot(x='time_step', y='base_vx', ax=axs[0], legend=False)
-# df.plot(x='time_step', y='base_vy', ax=axs[1], legend=False)
-# df.plot(x='time_step', y='base_vz', ax=axs[2], legend=False)
+    df.plot(x='time_step', y='base_vx', ax=axs[0], legend=False)
+    df.plot(x='time_step', y='base_vy', ax=axs[1], legend=False)
+    df.plot(x='time_step', y='base_wz', ax=axs[2], legend=False)
 
-# axs[0].set_xlabel('Time (s)')
-# axs[0].set_ylabel('$V_x$ (m/s)')
-# axs[0].grid(True)
+    axs[0].hlines(y=1, xmin=0, xmax=10, colors='r', linestyles='--', label='command vx')
+    axs[0].set_xlabel('Time (s)')
+    axs[0].set_ylabel('$V_x$ (m/s)')
+    axs[0].grid(True)
+    axs[0].legend()
 
-# axs[1].set_xlabel('Time (s)')
-# axs[1].set_ylabel('$V_y$ (m/s)')
-# axs[1].grid(True)
+    axs[1].hlines(y=0, xmin=0, xmax=10, colors='r', linestyles='--', label='command vy')
+    axs[1].set_xlabel('Time (s)')
+    axs[1].set_ylabel('$V_y$ (m/s)')
+    axs[1].grid(True)
+    axs[1].legend()
 
-# axs[2].set_xlabel('Time (s)')
-# axs[2].set_ylabel('$V_z$ (m/s)')
-# axs[2].grid(True)
+    axs[2].hlines(y=0, xmin=0, xmax=10, colors='r', linestyles='--', label='command wz')
+    axs[2].set_xlabel('Time (s)')
+    axs[2].set_ylabel('$w_z$ (m/s)')
+    axs[2].grid(True)
+    axs[2].legend()
 
-# plt.tight_layout()
-# plt.show()
+    plt.tight_layout()
+    plt.show()
 
-n_dof = len(DOF_NAMES)
-fig, axs = plt.subplots(5, 2, figsize=(10, 10))
+def plot_dof_torque():
+    n_dof = len(DOF_NAMES)
+    fig, axs = plt.subplots(5, 2, figsize=(10, 10))
 
-axs = axs.flatten()
-for index, key in enumerate(DOF_NAMES):
-    column_name = 'pos_' + key
-    df.plot(x='time_step', y=column_name, ax=axs[index], legend=False, linewidth=0.9)
-    axs[index].set_xlabel('Time (s)')
-    axs[index].set_ylabel(column_name)
-    axs[index].grid(True)
+    axs = axs.flatten()
+    for index, key in enumerate(DOF_NAMES):
+        column_name = 'torque_' + key
+        df.plot(x='time_step', y=column_name, ax=axs[index], legend=False, linewidth=0.9)
+        axs[index].set_xlabel('Time (s)')
+        axs[index].set_ylabel(column_name)
+        axs[index].grid(True)
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()    
+
+def plot_joint_positions():
+    n_dof = len(DOF_NAMES)
+    fig, axs = plt.subplots(5, 2, figsize=(10, 10))
+
+    axs = axs.flatten()
+    for index, key in enumerate(DOF_NAMES):
+        column_name = 'pos_' + key
+        df.plot(x='time_step', y=column_name, ax=axs[index], legend=False, linewidth=0.9)
+        axs[index].set_xlabel('Time (s)'); axs[index].set_ylabel(column_name)
+        axs[index].grid(True)
+
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    # plot_vel_tracking()
+    # plot_base_velocity()
+    plot_dof_torque()
